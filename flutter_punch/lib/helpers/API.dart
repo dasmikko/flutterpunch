c380925098668f7af2202f62d24574e263b07b3e
dasmikko <dasmikko@gmail.com>
2018-12-15 22:33:34 +0100

    Added API helper class

---
+++ flutter_punch/lib/helpers/API.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_punch/models/ForumsModel.dart';
+import 'package:flutter_punch/models/CategoryListModel.dart';
+import 'package:flutter_punch/models/ThreadListModel.dart';
+import 'dart:async';
+import 'dart:convert';
+
+class APIHelper {
+  final baseURL = 'https://facepunch-api-eu.herokuapp.com/';
+
+  Future<CategoryListModel> fetchCategories() async {
+    print('Fetching categories');
+    final response = await http.get(baseURL);
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON
+      return CategoryListModel.fromJson(json.decode(response.body));
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+  Future<ThreadListModel> fetchForum(String id) async {
+    print('Fetching categories');
+    final response = await http.get(baseURL + id);
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON
+      return ThreadListModel.fromJson(json.decode(response.body));
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+}
