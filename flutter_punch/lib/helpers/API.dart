0aa01e1af9f5d1871b58a548e32d87b61d908912
dasmikko <dasmikko@gmail.com>
2019-02-05 08:02:50 +0100

    Parse Http calls as utf8

--- flutter_punch/lib/helpers/API.dart
+++ flutter_punch/lib/helpers/API.dart
@@ -5,16 +5,17 @@ import 'package:flutter_punch/models/ThreadListModel.dart';
 import 'package:flutter_punch/models/PostListModel.dart';
 import 'dart:async';
 import 'dart:convert';
+import 'dart:convert' show utf8;
 
 class APIHelper {
   final baseURL = 'https://facepunch-api-eu.herokuapp.com/';
 
   Future<CategoryListModel> fetchCategories() async {
     print('Fetching categories');
-    final response = await http.get(baseURL);
+    final response = await http.get(baseURL, headers: {'Content-type': 'application/json; charset=utf-8'});
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON
-      return CategoryListModel.fromJson(json.decode(response.body));
+      return CategoryListModel.fromJson(json.decode(utf8.decode(response.bodyBytes)));
     } else {
       // If that response was not OK, throw an error.
       throw Exception('Failed to load post');
@@ -22,11 +23,11 @@ class APIHelper {
   }
 
   Future<ThreadListModel> fetchForum(String id) async {
-    print('Fetching categories');
-    final response = await http.get(baseURL + id);
+    print('Fetching forum');
+    final response = await http.get(baseURL + id, headers: {'Content-type': 'application/json; charset=utf-8'});
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON
-      return ThreadListModel.fromJson(json.decode(response.body));
+      return ThreadListModel.fromJson(json.decode(utf8.decode(response.bodyBytes)));
     } else {
       // If that response was not OK, throw an error.
       throw Exception('Failed to load post');
@@ -34,11 +35,11 @@ class APIHelper {
   }
 
   Future<PostListModel> fetchThread(String url) async {
-    print('Fetching categories');
-    final response = await http.get(baseURL + url.substring(1));
+    print('Fetching thread');
+    final response = await http.get(baseURL + url.substring(1), headers: {'Content-type': 'application/json; charset=utf-8'});
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON
-      return PostListModel.fromJson(json.decode(response.body));
+      return PostListModel.fromJson(json.decode(utf8.decode(response.bodyBytes)));
     } else {
       // If that response was not OK, throw an error.
       throw Exception('Failed to load post');
