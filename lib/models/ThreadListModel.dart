cca35fc626a0a47e73d49bab22980ea280747f72
dasmikko <dasmikko@gmail.com>
2019-02-07 16:18:39 +0100

    Handle the new json format

--- lib/models/ThreadListModel.dart
+++ lib/models/ThreadListModel.dart
@@ -1,14 +1,20 @@
 import 'package:flutter_punch/models/ThreadModel.dart';
 
 class ThreadListModel {
+  final int currentPage;
+  final int totalPages;
   final List<ThreadModel> threads;
-  ThreadListModel({this.threads});
+  ThreadListModel({this.threads, this.currentPage, this.totalPages});
 
-  factory ThreadListModel.fromJson(List<dynamic> parsedJson) {
-    List<ThreadModel> threads = parsedJson.map((i)=> ThreadModel.fromJson(i)).toList();
+  factory ThreadListModel.fromJson(Map<String, dynamic> parsedJson) {
+     var threadsList = parsedJson['threads'] as List;
+
+    List<ThreadModel> threads = threadsList.map((i)=> ThreadModel.fromJson(i)).toList();
 
     return new ThreadListModel(
-       threads: threads,
+      currentPage: parsedJson['currentPage'],
+      totalPages: parsedJson['totalPages'],
+      threads: threads,
     );
   }
 }
