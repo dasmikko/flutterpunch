f451c18e685830bde118208894680f7d756b2b3e
dasmikko <dasmikko@gmail.com>
2019-02-12 21:19:37 +0100

    Improve embed widget

--- lib/widgets/PostElements/Embed.dart
+++ lib/widgets/PostElements/Embed.dart
@@ -4,15 +4,63 @@ import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter_advanced_networkimage/provider.dart';
 import 'package:flutter_advanced_networkimage/transition.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
 
-class EmbedWidget extends StatelessWidget {
+class EmbedWidget extends StatefulWidget {
   String url;
 
   EmbedWidget(this.url);
 
+  @override
+  _EmbedWidgetState createState() => _EmbedWidgetState();
+}
+
+class _EmbedWidgetState extends State<EmbedWidget>
+    with AutomaticKeepAliveClientMixin {
+  String _title = "Fetching embed...";
+  String _description = "";
+  String _imageUrl;
+
+  @override
+  void initState () {
+    super.initState();
+    fetchHtml(widget.url);
+  }
+
+  void fetchHtml(url) {
+    http.get(url).then((response) {
+      if (response.statusCode == 200) {
+        // If server returns an OK response, parse the JSON
+        var document = parse(response.body);
+
+        var list = document.getElementsByTagName('meta');
+
+        setState(() {
+          for (var item in list) {
+            if (item.attributes['property'] == "og:title") {
+              _title = item.attributes['content'];
+            }
+
+            if (item.attributes['property'] == "og:description") {
+              _description = item.attributes['content'];
+            }
+
+            if (item.attributes['property'] == "og:image") {
+              _imageUrl = item.attributes['content'];
+            }
+          }
+        });
+      } else {
+        // If that response was not OK, throw an error.
+        throw Exception('Failed to load post');
+      }
+    });
+  }
+
   bool notNull(Object o) => o != null;
 
-  Widget handleImage (String url) {
+  Widget handleImage(String url) {
     return TransitionToImage(
       image: AdvancedNetworkImage(url),
       placeholder: CircularProgressIndicator(),
@@ -21,40 +69,29 @@ class EmbedWidget extends StatelessWidget {
   }
 
   @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
-    UrlEmbedModel _model = new UrlEmbedModel();
-
-    _model.fetchHtml(url);
-
     //UrlEmbedModel.of(context).fetchHtml(this.url);
-    return new ScopedModel<UrlEmbedModel>(
-      model: _model,
-      child: new ScopedModelDescendant<UrlEmbedModel>(
-        builder: (context, child, model) {
-          // Embed content
-          return Card(
-            clipBehavior: Clip.antiAlias,
-            margin: EdgeInsets.only(bottom: 12.0),
-            child: InkWell(
-              onTap: () async {
-                if (await canLaunch(url)) {
-                  await launch(url);
-                }
-              },
-              child: Column(
-                children: <Widget>[
-                  model.imageUrl != null
-                      ? handleImage(model.imageUrl)
-                      : null,
-                  ListTile(
-                    title: Text(model.title),
-                    subtitle: Text(model.description),
-                  ),
-                ].where(notNull).toList(),
-              ),
-            ),
-          );
+    return new Card(
+      clipBehavior: Clip.antiAlias,
+      margin: EdgeInsets.only(bottom: 12.0),
+      child: InkWell(
+        onTap: () async {
+          if (await canLaunch(widget.url)) {
+            await launch(widget.url);
+          }
         },
+        child: Column(
+          children: <Widget>[
+            _imageUrl != null ? handleImage(_imageUrl) : null,
+            ListTile(
+              title: Text(_title),
+              subtitle: Text(_description),
+            ),
+          ].where(notNull).toList(),
+        ),
       ),
     );
   }
